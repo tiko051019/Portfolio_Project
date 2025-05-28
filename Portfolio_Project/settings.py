0cf1180386a8c2a78df4d154cfbec66f82cdbdf4
Tigran <tikoabrahamyan05@gmail.com>
2025-05-28 18:37:49 +0400

    Update settings.py

--- Portfolio_Project/settings.py
+++ Portfolio_Project/settings.py
@@ -138,8 +138,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tikoabrahamyan05@gmail.com'
-EMAIL_HOST_PASSWORD = 'xnnkorsmhhlxuraw'
+EMAIL_HOST_USER = '...@gmail.com'
+EMAIL_HOST_PASSWORD = '...'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BECKEND = 'django.core.mail.backends.smtp.EmailBackend'
