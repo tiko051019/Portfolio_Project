5616fb2cc92148793ac3fe1252e3e44948cb6701
Tigran <tikoabrahamyan05@gmail.com>
2025-12-04 12:17:58 +0400

    Mask email password in settings.py
    Replaced the actual email password with a placeholder for security.

--- Portfolio_Project/settings.py
+++ Portfolio_Project/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'tikoabrahamyan05@gmail.com'
-EMAIL_HOST_PASSWORD = 'xnnkorsmhhlxuraw'
+EMAIL_HOST_PASSWORD = '****************'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BECKEND = 'django.core.mail.backends.smtp.EmailBackend'
